071bed5ab262e03b94d55794a5eefd702bb8f7c3
Fraih001 <90857197+Fraih001@users.noreply.github.com>
2023-05-24 14:41:58 -0400

    chapter 6 code additions

--- servy/lib/servy/handler.ex
+++ servy/lib/servy/handler.ex
@@ -6,6 +6,7 @@ defmodule Servy.Handler do
 
     request
     |> parse()
+    |> log()
     |> route()
     |> format_response()
   end
@@ -20,8 +21,18 @@ defmodule Servy.Handler do
     %{method: method, path: path, resp_body: ""}
   end
 
+  def log(conv), do: IO.inspect(conv)
+
   def route(conv) do
+    route(conv, conv.method, conv.path)
+  end
+
+  def route(conv, "GET", "/wildthings") do
     %{conv | resp_body: "Bears, Lions, Tigers"}
+  end
+
+  def route(conv, "GET", "/bears") do
+    %{conv | resp_body: "Teddy, Smokey, Paddington"}
   end
 
   def format_response(conv) do
@@ -48,6 +59,28 @@ Content-Type: text/html
 Content-Length: 20
 
 Bears, Lions, Tigers
+"""
+
+response = Servy.Handler.handle(request)
+
+IO.puts(response)
+
+request = """
+GET /bears HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+"""
+
+response = Servy.Handler.handle(request)
+
+IO.puts(response)
+
+request = """
+GET /bigfoot HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
 """
 
 response = Servy.Handler.handle(request)
