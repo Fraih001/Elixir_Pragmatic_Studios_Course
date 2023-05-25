d344ac580bdef2b27e321cd9d76906f24d55d1b1
Fraih001 <90857197+Fraih001@users.noreply.github.com>
2023-05-25 12:08:26 -0400

    chapter 9 code additions

--- servy/lib/servy/handler.ex
+++ servy/lib/servy/handler.ex
@@ -62,16 +62,29 @@ defmodule Servy.Handler do
     %{conv | status: 200, resp_body: "Teddy, Smokey, Paddington"}
   end
 
+  def route(%{method: "GET", path: "/bears/new"} = conv) do
+    Path.expand("../../pages", __DIR__)
+    |> Path.join("form.html")
+    |> File.read()
+    |> handle_file(conv)
+end
+
   def route(%{method: "GET", path: "/bears/" <> id} = conv) do
     %{conv | status: 200, resp_body: "Bear #{id}"}
   end
 
   def route(%{method: "GET", path: "/about"} = conv) do
-    file =
       Path.expand("../../pages", __DIR__)
       |> Path.join("about.html")
       |> File.read()
       |> handle_file(conv)
+  end
+
+  def route(%{method: "GET", path: "/pages/" <> file} = conv) do
+    Path.expand("../../pages", __DIR__)
+    |> Path.join(file <> ".html")
+    |> File.read
+    |> handle_file(conv)
   end
 
   def handle_file({:ok, content}, conv) do
@@ -235,6 +248,18 @@ IO.puts(response)
 
 request = """
 GET /about HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+response = Servy.Handler.handle(request)
+
+IO.puts(response)
+
+request = """
+GET /bears/new HTTP/1.1
 Host: example.com
 User-Agent: ExampleBrowser/1.0
 Accept: */*
