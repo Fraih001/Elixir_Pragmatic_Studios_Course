58cdfeacc7adb334d7486de453a4afec65ce44af
Fraih001 <90857197+Fraih001@users.noreply.github.com>
2023-06-06 09:05:01 -0400

    chapter 28 code additions

--- servy/lib/servy/kickstarter.ex
+++ servy/lib/servy/kickstarter.ex
@@ -1,15 +1,27 @@
 defmodule Servy.KickStarter do
   use GenServer
 
+  # Client Interface
+
   def start do
     IO.puts "Starting the kickstarter.. "
     GenServer.start(__MODULE__, :ok, name: __MODULE__)
   end
 
+  def get_server do
+    GenServer.call __MODULE__, :get_server
+  end
+
+  # Server Callbacks
+
   def init(:ok) do
     Process.flag(:trap_exit, true)
     server_pid = start_server()
     {:ok, server_pid}
+  end
+
+  def handle_call(:get_server, _from, state) do
+    {:reply, state, state}
   end
 
   def handle_info({:EXIT, _pid, reason}, _state) do
@@ -20,10 +32,10 @@ defmodule Servy.KickStarter do
 
   defp start_server do
     IO.puts "Starting the HTTP server... "
-    server_pid = spawn_link(Servy.HttpServer, :start, [4000])
+    spawn_link(Servy.HttpServer, :start, [4000])
     # server_pid = spawn(Servy.HttpServer, :start, [4000])
     # Process.link(server_pid)
-    Process.register(server_pid, :http_server)
-    server_pid
+    # Process.register(server_pid, :http_server)
+    # server_pid
   end
 end
